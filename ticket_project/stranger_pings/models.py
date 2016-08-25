3434006de0f45eb815c6833aee1153a17f9cb3dc
Megan Ford <meg.c.ford@gmail.com>
2016-08-25 15:25:44 -0500

    models are built, registered, and imported

--- ticket_project/stranger_pings/models.py
+++ ticket_project/stranger_pings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# NOTE: the 'User' class is already on the standard app, we don't need to make our own one.
+
+class Venue(models.Model):
+  name = models.CharField(max_length=200)
+  def __str__(self):
+    return self.name
+
+class Event(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.CharField(max_length=1000)
+  begin_date_time = models.DateTimeField("event start")
+  end_date_time = models.DateTimeField("event end")
+  tix_limit = models.IntegerField()
+  address = models.CharField(max_length=700)
+  venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+class UserEvent(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
+  creator = models.BooleanField()
+  def __str__(self):
+    return(str(self.user) + " attending " + str(self.event))
--- ticket_project/stranger_pings/views.py
+++ ticket_project/stranger_pings/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+
+from .models import Venue, Event, UserEvent
 
 # Create your views here.
 
