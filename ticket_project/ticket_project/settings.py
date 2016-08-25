6ad71c6107effa2f4c01ca196bd6b00035bf6803
Alan Eatherly <eatherlyae@gmail.com>
2016-08-25 14:25:04 -0500

    modified gitignore

--- ticket_project/ticket_project/settings.py
+++ ticket_project/ticket_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stranger_pings.app.stranger_pingsConfig',
 ]
 
 MIDDLEWARE = [
