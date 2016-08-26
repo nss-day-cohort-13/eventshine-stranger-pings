825cb7c034da26ed06dae75d935c30445cfba4cb
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-26 15:54:18 -0500

    Wrote data view for returning all users
    Commented a rough solution for returning events matching the current
    user

--- ticket_project/stranger_pings/views.py
+++ ticket_project/stranger_pings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from django.views import generic
+from django.core import serializers
 
 from .models import Venue, Event, UserEvent
 
@@ -9,3 +10,21 @@ from .models import Venue, Event, UserEvent
 
 class IndexView(generic.TemplateView):
   template_name = 'stranger_pings/index.html'
+
+def ReturnAllEvents(request):
+  events = Event.objects.all()
+  data = serializers.serialize('json', events)
+
+  return HttpResponse(data, content_type='application/json')
+
+# def ReturnUserEvents(request, user_id):
+#   user_events = UserEvent.objects.get(user=user_id)
+#   event_ids = []
+#   for ue in user_events:
+#     event_ids.append(ue.event)
+
+#   matched_events = []
+#   for key in event_ids:
+#     fetched = Event.objects.get(pk=key)
+#     matched_events.append(fetched)
+
