3dba3322344997b75f988f425e6b6596bb6a0f13
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-29 15:50:55 -0500

    Added comment about refreshing to event-detail.js

--- ticket_project/stranger_pings/static/app/controllers/event-detail-ctrl.js
+++ ticket_project/stranger_pings/static/app/controllers/event-detail-ctrl.js
@@ -1,5 +1,9 @@
 app.controller('EventDetailCtrl', function($routeParams, $location, AllEventFactory, VenueFactory) {
 
+  // FYI -- and this is a big one -- you cannot simply refresh the event detail page as of now.
+  // The controller depends on data that is resolved when you load all events. For now, you have to navigate
+  // back to the all events page and click through to the event you want to check out.
+
   const eventDetail = this;
 
   eventDetail.thisEvent = AllEventFactory.getSingleEvent(parseInt($routeParams.event));
