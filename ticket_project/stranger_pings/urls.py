698581065a4f1b8f71207e0b1faff1d6711022da
Alan Eatherly <eatherlyae@gmail.com>
2016-08-29 15:09:18 -0500

    register users working, need to fix redirect after login

--- ticket_project/stranger_pings/urls.py
+++ ticket_project/stranger_pings/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
 
   url(r'^$', generic.TemplateView.as_view(template_name="stranger_pings/home.html"), name="unloggedinhome"),
 
+  # Log In URLS
   url(r'^login/', generic.TemplateView.as_view(template_name="stranger_pings/login.html"), name='login'),
   url(r'^loggedin/', generic.TemplateView.as_view(template_name="stranger_pings/index.html"), name='index'),
   url(r'^login_user/$', views.login_user, name='login_user'),
@@ -23,6 +24,7 @@ urlpatterns = [
   url(r'^events/', include(event_patterns)),
   url(r'^venues/', include(venue_patterns)),
 
+  # Register URLS
   url(r'^register/', generic.TemplateView.as_view(template_name="stranger_pings/register.html")),
   url(r'^registered_user/$', views.create_user, name='create_user'),
 ]
