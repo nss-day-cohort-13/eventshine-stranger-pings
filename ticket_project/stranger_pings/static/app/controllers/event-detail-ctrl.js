fbe3a942a3f0ddb340679750555ccd9c717e9300
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-29 15:10:23 -0500

    All event info populates page correctly

--- ticket_project/stranger_pings/static/app/controllers/event-detail-ctrl.js
+++ ticket_project/stranger_pings/static/app/controllers/event-detail-ctrl.js
@@ -1,8 +1,16 @@
-app.controller('EventDetailCtrl', function($routeParams, AllEventFactory) {
+app.controller('EventDetailCtrl', function($routeParams, AllEventFactory, VenueFactory) {
 
-    const eventDetail = this;
+  const eventDetail = this;
 
-    eventDetail.thisEvent = AllEventFactory.getSingleEvent(parseInt($routeParams.event));
-    console.log("thisEvent: ", eventDetail.thisEvent);
+  eventDetail.thisEvent = AllEventFactory.getSingleEvent(parseInt($routeParams.event));
+  console.log("thisEvent: ", eventDetail.thisEvent);
+
+  eventDetail.getVenueName = (key) => {
+    let allVenues = VenueFactory.getAllVenues();
+    venue_filter = allVenues.filter((venue) => {
+      return venue.pk === key;
+    });
+    return venue_filter[0].fields.name;
+  }
 
 });
