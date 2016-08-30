b41334759d6dc9eae7391087f4883ea6bce7cd1b
Megan Ford <meg.c.ford@gmail.com>
2016-08-30 09:49:50 -0500

    completed the http post function in venue controller.

--- ticket_project/stranger_pings/static/app/controllers/Venue.js
+++ ticket_project/stranger_pings/static/app/controllers/Venue.js
@@ -1,5 +1,27 @@
 app.controller("Venue", function($scope, $http, $location, VenueFactory) {
   const venue = this;
 
-  venue.name = ""
+  venue.title="Create An Event";
+  // Form fills out this variable.
+  venue.name = "";
+
+  venue.completeCreation = () => {
+    // Sends a 'POST' of the form data to views.py database, through the urls.py. See app.py for the headers config for the 'content-type'.
+    $http({
+      url: "http//localhost:8000/stranger_pings/events/venue/", 
+      method: "POST", 
+      headers: {"Content-Type": "application/x-www-form-urlencoded"}, 
+      data: {"name": venue.name}
+    })
+    .success(() => {
+    // TODO: sign the user up for the event after the event is created.
+      venue.goToMyEvents();
+    });
+  };
+
+  venue.goToMyEvents = () => {
+    // Just a path redirect.
+    $location.path(`/myevents`);
+  };
+
 });
