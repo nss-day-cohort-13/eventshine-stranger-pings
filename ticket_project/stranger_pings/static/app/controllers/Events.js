ed5ce780f89c682ed2bbf24bd072cf832df7d9d0
Megan Ford <meg.c.ford@gmail.com>
2016-08-31 11:30:39 -0500

    FIXED THE VIEW EVENTS

--- ticket_project/stranger_pings/static/app/controllers/Events.js
+++ ticket_project/stranger_pings/static/app/controllers/Events.js
@@ -14,22 +14,18 @@ app.controller("Events", function($scope, $http, $location, AllEventsFactory, Ve
       AllEventsFactory.setAllEvents(res.data);
     });
 
-
-  allEvents.title = "Events";
-
   allEvents.getVenueName = (key) => {
     venue_filter = $scope.venues.filter((venue) => {
       return venue.pk === key;
     });
     return venue_filter[0].fields.name;
-  }
+  };
 
   allEvents.goToHome = () => {
     window.location.assign('/logout/');
   };
 
   allEvents.detailBack = (allOrMy) =>{
-    console.log("going back. will pass whether the user clicked 'my events' or 'all events' back button here.");
     if (allOrMy === "all") {
       $location.path(`/events`);
     }
